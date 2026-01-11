d02af99d68a3d24c1fbc9f6e6bf9861eb5d85c26
ROG <luan.roger.2003@gmail.com>
2026-01-11 13:39:47 -0300

    refactor: get projects from constants

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,31 @@
+import type { Metadata } from "next";
+import Link from "@/components/link";
 import ProjectDocCard from "@/components/project-doc-card";
 import Reveal from "@/components/reveal";
 import Squares from "@/components/squares-bg";
+import { projects } from "./constants";
+
+export const metadata: Metadata = {
+  title: "docs.",
+  description: "Documentation of Luan Roger's open-source projects",
+};
 
 export default function HomePage() {
   return (
     <div className="relative size-full flex-1 rounded-md border border-border">
-      <div className="absolute inset-x-0 top-1/2 flex -translate-y-1/2 flex-col items-center justify-center gap-4">
+      <div className="absolute inset-x-0 top-1/2 flex -translate-y-1/2 flex-col items-center justify-center gap-4 p-4">
         <h1 className="text-5xl">docs.</h1>
         <p className="text-muted-foreground text-sm">
-          Documentation for Luan Roger's open-source projects
+          Documentation of{" "}
+          <Link href="https://github.com/LuanRoger" isExternal>
+            Luan Roger's
+          </Link>{" "}
+          open-source projects
         </p>
         <div className="grid grid-cols-1 items-center justify-center gap-5 md:grid-cols-2 lg:grid-cols-3">
-          <ProjectDocCard
-            description="Electron Forge with shadcn-ui (Vite + Typescript)"
-            documentationLink="/electron-shadcn"
-            name="electron-shadcn"
-            repositoryLink="https://github.com/LuanRoger/electron-shadcn"
-          />
+          {projects.map((project) => (
+            <ProjectDocCard key={project.name} {...project} />
+          ))}
         </div>
       </div>
       <Reveal className="absolute inset-x-0 -z-10 size-full">
